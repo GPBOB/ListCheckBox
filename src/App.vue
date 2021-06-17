4a18c2d857c39b1e6e2d80f795ccdd00df8ca2c3
gp <812117994@qq.com>
2021-06-17 17:19:28 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="content">
+    <ListCheckBox
+      :selectAll="false"
+      :data="data"
+      @select="selectData"
+    >
+      <template v-slot:checkSlot="item">
+        <div class="test">
+          <p>{{ item.data.name }}</p>
+          <p>{{ item.data.value }}</p>
+        </div>
+      </template>
+      <template v-slot:customBtn>
+      </template>
+    </ListCheckBox>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+// import ListCheckBox from "./components/ListCheckBox.vue";
+import ListCheckBox from './components/ListCheckBox'
+Vue.use(ListCheckBox)
+export default {
+  data() {
+    return {
+      data: [
+        {
+          name: "名称一",
+          value: "可自定义内容",
+          selected: true,
+        },
+        {
+          name: "名称二",
+          value: 2,
+          disabled: true,
+          selected: true,
+        },
+        {
+          name: "名称三",
+          value: 3,
+          disabled: true,
+        },
+        {
+          name: "名称四",
+          value: 4,
+        },
+      ],
+    };
+  },
+  components: {
+    // ListCheckBox,
+  },
+  methods: {
+    selectData(data) {
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style>
+.content {
+  padding: 40rpx;
+}
+
+.test {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/ListCheckBox.vue
@@ -0,0 +1,266 @@
+<template>
+  <div class="lcb-container">
+    <div class="lcb-all">
+      <div
+        v-if="!disabledAll"
+        class="lcb-check-btn"
+        :style="{
+          'border-radius': type == 'circle' && '50%',
+          'border-color': linkBorder && defaultColor,
+        }"
+        @click="selectAllData"
+      >
+        <div
+          v-if="isSelectAll && type != 'custom'"
+          class="lcb-check-btn-active"
+          :style="{
+            'background-color': defaultColor,
+            'border-radius': type == 'circle' && '50%',
+          }"
+        ></div>
+        <slot v-if="isSelectAll && type == 'custom'" name="customBtn"></slot>
+      </div>
+      <div
+        v-if="disabledAll"
+        class="lcb-check-btn"
+        :style="{ 'border-radius': type == 'circle' && '50%' }"
+      >
+        <div
+          :class="[
+            'lcb-check-btn-disabled',
+            isSelectAll && 'lcb-check-btn-disabled-check',
+          ]"
+          :style="{ 'border-radius': type == 'circle' && '50%' }"
+        ></div>
+      </div>
+      <p>{{ allText }}</p>
+    </div>
+    <div class="lcb-content">
+      <div class="lcb-item" v-for="(item, index) in checkData" :key="index">
+        <div
+          v-if="!item.disabled"
+          class="lcb-check-btn"
+          :style="{
+            'border-radius': type == 'circle' && '50%',
+            'border-color': linkBorder && defaultColor,
+          }"
+          @click="select(item, index)"
+        >
+          <div
+            v-if="item.selected && type != 'custom'"
+            class="lcb-check-btn-active"
+            :style="{
+              'background-color': defaultColor,
+              'border-radius': type == 'circle' && '50%',
+            }"
+          ></div>
+          <slot
+            v-if="item.selected && type == 'custom'"
+            name="customBtn"
+          ></slot>
+        </div>
+
+        <div
+          v-if="item.disabled"
+          class="lcb-check-btn"
+          :style="{ 'border-radius': type == 'circle' && '50%' }"
+        >
+          <div
+            :class="[
+              'lcb-check-btn-disabled',
+              item.selected && 'lcb-check-btn-disabled-check',
+            ]"
+            :style="{ 'border-radius': type == 'circle' && '50%' }"
+          ></div>
+          <!-- <slot v-if="type=='custom'" name="customDisabledBtn"></slot> -->
+        </div>
+        <slot name="checkSlot" :data="data[index]"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListCheckBox",
+  data() {
+    return {
+      selectIndexData: [], //选中index数据
+      selectData: [], //选中数据
+      isSelectAll: false,
+      selectLength: 0, //可选数据长度
+      defaultSelectData: [], // 默认选中并且设置disabled为true的数据
+    };
+  },
+  props: {
+    //默认主题颜色
+    defaultColor: {
+      type: String,
+      default: "#1989fa",
+    },
+    //复选框边界和配置色统一
+    linkBorder: {
+      type: Boolean,
+      default: true,
+    },
+    //选择按钮类型 pane-方框，circle-圆形，custom-自定义
+    type: {
+      type: String,
+      default: "pane",
+    },
+    //是否全选
+    selectAll: {
+      type: Boolean,
+      default: false,
+    },
+    //禁止全选
+    disabledAll: {
+      type: Boolean,
+      default: false,
+    },
+    //数据
+    data: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    //全选text
+    allText: {
+      type: String,
+      default: "全选",
+    },
+  },
+  computed: {
+    checkData() {
+      let datas = [...this.data];
+      datas.map((el) => {
+        if (el.selected == undefined) {
+          el.selected = false;
+        }
+        if (el.disabled == undefined) {
+          el.disabled = false;
+        }
+        return el;
+      });
+      return datas;
+    },
+  },
+  methods: {
+    select(data, index) {
+      let selIndex = this.selectIndexData.indexOf(index);
+      let selData = data;
+      if (selIndex == -1) {
+        this.checkData[index].selected = true;
+        this.selectData.push(selData);
+        this.selectIndexData.push(index);
+      } else {
+        this.selectIndexData.splice(selIndex, 1);
+        this.selectData.splice(selIndex, 1);
+        this.checkData[index].selected = false;
+      }
+      if (this.selectIndexData.length == this.selectLength) {
+        this.isSelectAll = true;
+      } else {
+        this.isSelectAll = false;
+      }
+      this.$forceUpdate();
+      this.$emit("select", this.selectData);
+    },
+    selectAllData() {
+      this.isSelectAll = !this.isSelectAll;
+      if (this.isSelectAll) {
+        this.checkData.map((el, index) => {
+          if (this.selectIndexData.indexOf(index) == -1) {
+            this.selectIndexData.push(index);
+            el.selected = true;
+          }
+          return el;
+        });
+        this.selectData = [...this.checkData];
+      } else {
+        this.checkData.map((el) => {
+          el.selected = false;
+          return el;
+        });
+        this.selectData = [];
+        this.selectIndexData = [];
+      }
+      this.$forceUpdate();
+      this.$emit("select", this.selectData);
+    },
+  },
+  mounted() {
+    this.isSelectAll = this.selectAll;
+    let i = 0;
+    this.data.forEach((el, index) => {
+      el.lcb_id = index;
+      // 配置全选，计算总的能被选中数
+      if (this.selectAll) {
+        if (!el.disabled || el.selected) {
+          el.selected = true;
+          i++;
+          this.selectData.push(el);
+          this.selectIndexData.push(index);
+        }
+        this.selectLength = i;
+      } else {
+        this.selectLength = this.data.length;
+        if (el.selected) {
+          this.selectData.push(el);
+          this.selectIndexData.push(index);
+        }
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+.lcb-check-btn {
+  width: 15px;
+  height: 15px;
+  border: 1px solid #b8b9ba;
+  margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 3px;
+}
+
+.lcb-check-btn-active {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: #000;
+}
+
+.lcb-check-btn-disabled {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  background-color: #dddddd;
+}
+
+.lcb-check-btn-disabled-check {
+  width: 10px;
+  height: 10px;
+}
+.lcb-all {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #e3e3e3;
+  padding-bottom: 15px;
+}
+
+.lcb-content {
+  display: flex;
+  flex-direction: column;
+}
+.lcb-item {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #e3e3e3;
+  padding: 15px 0px;
+}
+</style>
